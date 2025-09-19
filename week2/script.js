80628ad3527de43d0ed4a97fe9710fe4745a8acb
Zunayed Ahmed <51515519+zunayeddd@users.noreply.github.com>
2025-09-19 20:03:18 +0300

    Update script.js

--- week2/script.js
+++ week2/script.js
@@ -18,24 +18,3 @@ window.onload = async function() {
         // Error message already set in data.js
     }
 };
-
-// Populate the movies dropdown with sorted movie titles
-function populateMoviesDropdown() {
-    const selectElement = document.getElementById('movie-select');
-    
-    // Clear existing options except the first placeholder
-    while (selectElement.options.length > 1) {
-        selectElement.remove(1);
-    }
-    
-    // Sort movies alphabetically by title
-    const sortedMovies = [...movies].sort((a, b) => a.title.localeCompare(b.title));
-    
-    // Add movies to dropdown
-    sortedMovies.forEach(movie => {
-        const option = document.createElement('option');
-        option.value = movie.id;
-        option.textContent = movie.title;
-        selectElement.appendChild(option);
-    });
-}
